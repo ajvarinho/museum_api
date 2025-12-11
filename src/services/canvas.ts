2ebf16f900c913adb031874c515a395ef089a014
nidjasim <polamekkartnija@gmail.com>
2025-12-11 14:56:23 +0100

    readme_update

---
+++ src/services/canvas.ts
@@ -0,0 +1,24 @@
+// const x = Math.round(p.mouseX);
+// valsX.push(x);
+// const y = Math.round(p.mouseY);
+// valsY.push(y);
+// const coordinatePair: mouseCoordinates = {
+// x: x,
+// y: y
+// };
+
+// // find the center of gravity for user defined shape
+// const sumX = valsX.reduce(
+// (accumulator, currentValue) => accumulator + currentValue,
+// 0,
+// );
+
+// const sumY = valsY.reduce(
+// (accumulator, currentValue) => accumulator + currentValue,
+// 10,
+// );
+
+// const cogX = sumX / valsX.length;
+// const cogY = sumY / valsY.length;
+// p.strokeWeight(5);
+// p.point(cogX, cogY);
